2bf8bf149ce9521cbeb8c0712b1076415e75d4f3
petermlm <pedromelgueira@gmail.com>
2014-06-14 15:52:23 +0100

    Added method for multiple instantiation of variables in a distribution

--- src/dist.py
+++ src/dist.py
@@ -1,3 +1,5 @@
+import copy
+
 class RandVar:
     def __init__(self, name, domain):
         self.name = name
@@ -10,6 +12,12 @@ class Dist:
     def __init__(self, rand_vars, values):
         self.rand_vars = rand_vars
         self.values = values
+
+    def varInDist(self, rand_var):
+        for i in self.rand_vars:
+            if i.name == rand_vars.name:
+                return True
+        return False
 
     def mult(self, dist):
         f = lambda x, y: x*y
@@ -66,15 +74,19 @@ class Dist:
             res_values_size *= len(i.domain)
 
         # Calculate resulting distribution
-        target1 = 0
+        index1 = 0
         for i in range(res_values_size):
-            target2 = 0
+            # Get index 2
+            index2 = 0
             for j in range(len(dist.rand_vars)):
-                target2 += ((i / div[j]) % len(dist.rand_vars[j].domain)) * mult[j]
+                dim = len(dist.rand_vars[j].domain)
+                index2 += ((i / div[j]) % dim) * mult[j]
 
-            res_values.append(fun(self.values[target1], dist.values[target2]))
+            # Calculate value
+            res_values.append(fun(self.values[index1], dist.values[index2]))
 
-            target1 = (target1 + 1) % len(self.values)
+            # Increment index 1
+            index1 = (index1 + 1) % len(self.values)
 
         # Make Dist object and return
         return Dist(res_rand_vars, res_values)
@@ -95,34 +107,68 @@ class Dist:
                 res_rand_vars.append(i)
 
         # Calculate resulting size of distribution
-        res_values_size = 1
-        for i in res_rand_vars:
-            res_values_size *= len(i.domain)
+        res_values_size = self.getValuesListSize(res_rand_vars)
 
         # Initialized Resulting distribution
         res_values = [0] * res_values_size
 
         # Calculate marginal
         for i in range(len(self.values)):
-            target = 0
+            index = 0
             div = 1
             mult = 1
             k = 0
 
             for j in self.rand_vars:
                 if k < len(res_rand_vars) and j.name == res_rand_vars[k].name:
-                    target += ((i / div) % len(res_rand_vars[k].domain)) * mult
+                    index += ((i / div) % len(res_rand_vars[k].domain)) * mult
                     mult *= len(res_rand_vars[k].domain)
                     k += 1
 
                 div *= len(j.domain)
 
-            res_values[target] += self.values[i]
+            res_values[index] += self.values[i]
 
         # Make Dist object and return
         return Dist(res_rand_vars, res_values)
 
-    def instVar(self, rand_var, inst):
+    def instVar(self, rand_vars, insts):
+        # If this is a single instantiation
+        if type(rand_vars) != list and type(insts) != list:
+            return self.instVarSingle(rand_vars, insts)
+
+        # If this is a multiple instantiation
+        elif type(rand_vars) == list and type(insts) == list:
+            # If the size of this lists is not the same return
+            if len(rand_vars) != len(insts):
+                return None
+
+            # Instantiate one variable at a time
+            res = copy.copy(self)
+            for i in range(len(rand_vars)):
+                res = res.instVarSingle(rand_vars[i], insts[i])
+
+            return res
+
+    def instVarSingle(self, rand_var, inst):
+        res_rand_vars = []
+        var_index = -1
+
+        # Get resulting variables and var_index
+        for i in range(len(self.rand_vars)):
+            # Store current index of variable to instantiate and add rest of variables
+            if self.rand_vars[i].name == rand_var:
+                var_index = i
+                res_rand_vars += self.rand_vars[i+1:]
+                break
+
+            # Add current variable to list
+            res_rand_vars.append(self.rand_vars[i])
+
+        # If inst variable not in this distribution, return it unchanged
+        if var_index == -1:
+            return self
+
         # Get div factor
         div = 1
         for i in self.rand_vars:
@@ -130,23 +176,13 @@ class Dist:
                 break
             div *= len(i.domain)
 
-        # Get resulting variables and var_index
-        res_rand_vars = []
-        var_index = 0
-        for i in range(len(self.rand_vars)):
-            if self.rand_vars[i].name == rand_var:
-                res_rand_vars += self.rand_vars[i+1:]
-                var_index = i
-                break
-            res_rand_vars.append(self.rand_vars[i])
-
         # Get inst index
         for i in range(len(self.rand_vars[var_index].domain)):
             if self.rand_vars[var_index].domain[i] == inst:
                 inst_index = i
                 break
 
-        # Calculate resulting
+        # Calculate resulting distribution
         res_values = []
         for i in range(len(self.values)):
             if (i/div) % len(self.rand_vars[var_index].domain) == inst_index:
@@ -161,4 +197,10 @@ class Dist:
 
         # Make division
         return self.div(marg)
+
+    def getValuesListSize(self, rand_vars):
+        values_size = 1
+        for i in rand_vars:
+            values_size *= len(i.domain)
+        return values_size
 
