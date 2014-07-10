f3a83fc2f678840c417878dcf4768e3dddc492f1
petermlm <pedromelgueira@gmail.com>
2014-07-10 10:44:45 +0100

    Added tests for entropy

---
+++ ProbPy/tests/factor_inf_theory_tests.py
@@ -0,0 +1,55 @@
+from nose.tools import with_setup, nottest, assert_almost_equal
+from ProbPy.prob.rand_var import RandVar
+from ProbPy.prob.factor import Factor
+from ProbPy.prob.inf_theory import *
+
+from ProbPy.tests.factor_base import FactorBase
+import math
+
+
+class TestInfTheoryValue(FactorBase):
+    def __init__(self):
+        FactorBase.__init__(self)
+
+        # factors for information theory testing
+        self.fX1 = Factor(self.X, [0.5, 0.5])
+        self.fX2 = Factor(self.X, [0.0, 1.0])
+        self.fX3 = Factor(self.X, [1.0, 0.0])
+
+        self.fXY1 = Factor([self.X, self.Y], [0.25, 0.25, 0.25, 0.25])
+        self.fXY2 = Factor([self.X, self.Y], [0.1, 0.2, 0.3, 0.4])
+        self.fXY3 = Factor([self.X, self.Y], [0.4, 0.3, 0.2, 0.1])
+
+    def inf_theory_test_0(self):
+        """
+        H(X)
+        """
+
+        assert_almost_equal(entropy(self.fX1), 1.0, places=1)
+        assert_almost_equal(entropy(self.fX1, 10), 0.30103, places=5)
+        assert_almost_equal(entropy(self.fX1, math.e), 0.69315, places=5)
+
+        assert_almost_equal(entropy(self.fX2), 0.0, places=1)
+        assert_almost_equal(entropy(self.fX2, 10), 0.0, places=1)
+        assert_almost_equal(entropy(self.fX2, math.e), 0.0, places=1)
+
+        assert_almost_equal(entropy(self.fX3), 0.0, places=1)
+        assert_almost_equal(entropy(self.fX3, 10), 0.0, places=1)
+        assert_almost_equal(entropy(self.fX3, math.e), 0.0, places=1)
+
+    def inf_theory_test_1(self):
+        """
+        H(X, Y)
+        """
+
+        assert_almost_equal(entropy(self.fXY1), 2.0, places=1)
+        assert_almost_equal(entropy(self.fXY1, 10), 0.60206, places=5)
+        assert_almost_equal(entropy(self.fXY1, math.e), 1.3863, places=4)
+
+        assert_almost_equal(entropy(self.fXY2), 1.8464, places=4)
+        assert_almost_equal(entropy(self.fXY2, 10), 0.55583, places=5)
+        assert_almost_equal(entropy(self.fXY2, math.e), 1.2799, places=4)
+
+        assert_almost_equal(entropy(self.fXY3), 1.8464, places=4)
+        assert_almost_equal(entropy(self.fXY3, 10), 0.55583, places=5)
+        assert_almost_equal(entropy(self.fXY3, math.e), 1.2799, places=4)
