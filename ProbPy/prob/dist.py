f876c63768d5b0999c9926b69e80bd59647c9ea5
petermlm <pedromelgueira@gmail.com>
2014-07-14 11:01:32 +0100

    Added constructor for dist which uses a function instead of a factor

--- ProbPy/prob/dist.py
+++ ProbPy/prob/dist.py
@@ -51,8 +51,68 @@ class Dist:
 
         # If there is no factor
         elif left_vars is not None and values is not None:
-            # TODO
-            pass
+            # Make left vars list
+            rand_vars = []
+            if type(left_vars) == RandVar:
+                self.left_vars = [left_vars]
+            elif type(left_vars) == list:
+                self.left_vars = left_vars[:]
+            else:
+                raise Exception
+
+            # Make right vars list
+            if right_vars is not None:
+                if type(right_vars) == RandVar:
+                    self.right_vars = [right_vars]
+                elif type(right_vars) == list:
+                    self.right_vars = right_vars[:]
+                else:
+                    raise Exception
+            else:
+                self.right_vars = []
+
+            # Make rand_vars list for factor
+            rand_vars = self.left_vars + self.right_vars
+
+            # Build values array if it is not a list
+            if type(values) is not list:
+                values = self.makeValuesFromFunction(rand_vars, values)
+
+            # Make factor
+            self.factor = Factor(rand_vars, values)
 
         else:
             raise Exception
+
+    def makeValuesFromFunction(self, rand_vars, values):
+        # Initialize indexes
+        ind = [len(i.domain) for i in rand_vars]
+        cind = [0] * len(rand_vars)
+        cval = [i.domain[0] for i in rand_vars]
+
+        # Increment cind and cval to next value
+        def doInd(inc):
+            # If inc is out of the bounds of the indexes, the end was reached
+            if inc >= len(ind):
+                return False
+
+            # Move cind[inc] to next value
+            cind[inc] += 1
+
+            # If current cind is at the end, restart it and increment next
+            if cind[inc] == ind[inc]:
+                cind[inc] = 0
+                cval[inc] = rand_vars[inc].domain[0]
+                return doInd(inc+1)
+
+            # Also move the cval index
+            cval[inc] = rand_vars[inc].domain[cind[inc]]
+
+            return True
+
+        # Make values array
+        res = [values(*cval)]
+        while doInd(0):
+            res.append(values(*cval))
+
+        return res
