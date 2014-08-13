fbd3b6ad6413039a7b595ae27ff2c61cf008dce9
petermlm <pedromelgueira@gmail.com>
2014-08-13 16:28:46 +0100

    New setup script

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup
+import sys
+
+sys.path.append("ProbPy")
+import ProbPy
 
 setup(name="ProbPy",
       version="1.0",
-      description="Probability Calculus Library",
+      description="Multi Variable Probability Calculus Library",
       long_description=open('README.md').read(),
       license='LICENSE',
 
@@ -13,5 +17,24 @@ setup(name="ProbPy",
 
       url="https://github.com/petermlm/ProbPy",
 
-      packages=["ProbPy"],
+      package_dir={"": "ProbPy"},
+      py_modules['ProbPy']
+      provides['ProbPy']
+
+      keywords='probability calculus random variable bayes bayesian network \
+               information theory'
+      license="MIT License"
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Education',
+                   'Intended Audience :: End Users/Desktop',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: MIT License',
+                   'Natural Language :: English',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Software Development'
+                   ]
       )
