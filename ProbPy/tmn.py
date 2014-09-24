c7dca8a7e3cb074786fc23cab12f68dabaece73a
petermlm <pedromelgueira@gmail.com>
2014-09-24 15:34:12 +0100

    Implemented observations to the sumProduct algorithm

--- ProbPy/tmn.py
+++ ProbPy/tmn.py
@@ -1,4 +1,4 @@
-from ProbPy import MarkovNetwork
+from ProbPy import MarkovNetwork, Event
 
 class TreeMarkovNetwork(MarkovNetwork):
     """
@@ -14,13 +14,16 @@ class TreeMarkovNetwork(MarkovNetwork):
 
         # TODO, nothing in this class assures that the network is a tree, yet
 
-    def sumProduct(self, query_var):
+    def sumProduct(self, query_var, observed=Event()):
         """
         An implementation of the Sum Product algorithms, also known as Belief
         propagation algorithm. The algorithm is used to infer the distribution
         of one query variable in a Markov Network
 
         :param query_var: RandVar Instance which is going to be inferred
+        :param observed:  An event, instance of Event class, that represents
+                          the observations made to the Markov Network. It may
+                          be empty
         """
 
         # Get node of this var
@@ -30,22 +33,23 @@ class TreeMarkovNetwork(MarkovNetwork):
         neighbor_factors = node.neighbors
 
         # Calculate product of neighboring messages to this node
-        res = self.factorToVar(neighbor_factors[0], node.var)
+        res = self.factorToVar(neighbor_factors[0], node.var, observed)
         for i in neighbor_factors[1:]:
-            res *= self.factorToVar(i, node.var)
+            res *= self.factorToVar(i, node.var, observed)
 
         return res
 
-    def factorToVar(self, node, var):
+    def factorToVar(self, node, var, observed):
         """
         Used by sumProduct, this method is used to calculate the messages that
         are passed from factor nodes to the variable node in the arguments.
 
-        :param node: A MarkovNetFactor node. The method will calculate the
-                     message that this node passes to the variable in the var
-                     argument
-        :param var:  Instance of RandVar. The calculated message will pass to
-                     the node representing this variable
+        :param node:     A MarkovNetFactor node. The method will calculate the
+                         message that this node passes to the variable in the
+                         var argument
+        :param var:      Instance of RandVar. The calculated message will pass
+                         to the node representing this variable
+        :param observed: An event sent from the sumProduct method
         """
 
         # Get neighboring var nodes
@@ -54,7 +58,7 @@ class TreeMarkovNetwork(MarkovNetwork):
         # Calculate product of neighboring messages to this node
         res = None
         for i in neighbor_vars:
-            vf_res = self.varToFactor(i, node.factor)
+            vf_res = self.varToFactor(i, node.factor, observed)
             if vf_res is not None:
                 if res is None:
                     res = vf_res
@@ -69,18 +73,19 @@ class TreeMarkovNetwork(MarkovNetwork):
             res *= node.factor
 
         # Get marginal and return resulting message
-        return res.marginal(var)
+        return res.instVar(observed).marginal(var)
 
-    def varToFactor(self, node, factor):
+    def varToFactor(self, node, factor, observed):
         """
         Used by sumProduct, this method is used to calculate the messages that
         are passed from variable nodes to the factor node in the arguments.
 
-        :param node:   A MarkovNetVar node. The method will calculate the
-                       message that this node passes to the factor in the
-                       factor argument
-        :param factor: Instance of Factor. The calculated message will pass to
-                       the node representing this factor
+        :param node:     A MarkovNetVar node. The method will calculate the
+                         message that this node passes to the factor in the
+                         factor argument
+        :param factor:   Instance of Factor. The calculated message will pass
+                         to the node representing this factor
+        :param observed: An event sent from the sumProduct method
         """
 
         # Get neighboring factor nodes, except for argument node
@@ -91,8 +96,8 @@ class TreeMarkovNetwork(MarkovNetwork):
             return None
 
         # Otherwise, make the necessary products and return
-        res = self.factorToVar(neighbor_factors[0], node.var)
+        res = self.factorToVar(neighbor_factors[0], node.var, observed)
         for i in neighbor_factors[1:]:
-            res *= self.factorToVar(i, node.var)
+            res *= self.factorToVar(i, node.var, observed)
 
         return res
